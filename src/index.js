35edd113f7d096fbb7dbadcf6a2dce751a1b5aa3
guyriddler <guyriddler@gmail.com>
2024-03-15 18:55:59 -0500

    Profile Card Finished Assignment

--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <img className="avatar" src="guy_riddler.jpg" alt="Guy Stewart Riddler" />
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Guy Stewart Riddler</h1>
+      <p>
+        Full-stack web developer. Emplyed the past 26 years at the 22nd Circuit
+        Court City of St. Louis where he served as a their computer instructor,
+        then rehired as their application developer. I like video gaming,
+        playing golf, and enjoying activities with my friends.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="Java" emoji="💪" color="blue" textcolor="white" />
+      <Skill skill="Javascript" emoji="💪" color="orange" />
+      <Skill skill="React" emoji="💪" color="yellow" />
+      <Skill skill="HTML+CSS" emoji="💪" color="orangered" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div
+      className="skill"
+      style={{ backgroundColor: props.color, color: props.textcolor }}
+    >
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
